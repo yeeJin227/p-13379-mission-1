1d71e81b6672bdd5a799af4f78349b908c75548d
yeeJin227 <dpwls99600@gmail.com>
2025-06-02 15:24:32 +0900

    t9

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,20 +2,28 @@ package com.back;
 
 public class Calc {
     public static int run(String expression){
-        if(expression.equals("10 + 20 + 30")) return 60;
-        if(expression.equals("10 - 20 + 30")) return 20;
+        int result = 0;
 
         String[] expressionBits = expression.split(" ");
 
         int num1 = Integer.parseInt(expressionBits[0]);
         int num2 = Integer.parseInt(expressionBits[2]);
 
-        if (expressionBits[1].equals("+")) {
-            return num1+num2;
-        }else if(expressionBits[1].equals("-")){
-            return num1-num2;
+        if(expressionBits[1].equals("+")){
+            result =  num1+num2;
+        } else if(expressionBits[1].equals("-")){
+            result = num1-num2;
         }
 
-        return 0;
+        for(int i = 3; i<expressionBits.length; i++){
+            if (expressionBits[i].equals("+")) {
+                result += Integer.parseInt(expressionBits[i+1]);
+            } else if(expressionBits[i].equals("-")){
+                result -= Integer.parseInt(expressionBits[i+1]);
+            }
+        }
+
+
+        return result;
     }
 }
